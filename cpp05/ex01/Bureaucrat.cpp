eb04f6f080a83b5a7ee0119745a6554c703cb638
NatsuhaKoishi <ethanyap8@gmail.com>
2025-04-09 16:36:40 +0800

    ex01 Bureaucrat.cpp fix

--- cpp05/ex01/Bureaucrat.cpp
+++ cpp05/ex01/Bureaucrat.cpp
@@ -6,7 +6,7 @@
 /*   By: yyean-wa < yyean-wa@student.42kl.edu.my    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/23 16:32:42 by yyean-wa          #+#    #+#             */
-/*   Updated: 2025/04/03 16:33:29 by yyean-wa         ###   ########.fr       */
+/*   Updated: 2025/04/07 17:09:03 by yyean-wa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -91,5 +91,13 @@ std::ostream &operator << (std::ostream &fout, const Bureaucrat &obj)
 
 void	Bureaucrat::signForm(Form &form)
 {
-	form.beSigned(*this);
+	try
+	{
+		form.beSigned(*this);
+		std::cout << this->name << " successfully signed " << form.getName() << std::endl;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << this->name << " couldn't sign " << form.getName() << " because " << e.what() << std::endl;
+	}
 }
